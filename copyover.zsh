5cd3942fed59e040427e48e25b047d2371cda829
anseljohn <jjohnanselmo@gmail.com>
2020-12-05 13:31:14 -0600

    Worked, copied over files that weren't yet copied over

--- copyover.zsh
+++ copyover.zsh
@@ -1,23 +1,31 @@
 #!/bin/zsh
 
-echo "Copying pictures..."
-for dir in ~/Documents/vids/raw/drone/*; do
-	FILE="$(basename $dir)";
-	if [ -e /Volumes/WD_BLACK/Cinema/Drone/Pics/"$FILE" ]; then
-	    echo "$FILE exists"
-	else
-		if [ ${dor: -4} == ".JPG" ];
-		then
-			cp -v "$dir" /Volumes/WD_BLACK/Cinema/Drone/Vids/"${$(basename $dir)%.JPG}".JPG
-		fi
-		if [ ${dir: -4} == ".MP4" ];
-		then
-			cp -v "$dir" /Volumes/WD_BLACK/Cinema/Drone/Vids/"${$(basename $dir)%.MP4}".MP4		
-		fi 
-		
-		echo "$FILE does not exist"
-	fi
-done
-echo "\n";
+WD_BLACK=/Volumes/WD_BLACK
 
+if [[ -d "$WD_BLACK" ]]
+then
+	echo "*** FOUND WD_BLACK LOCATION ***"
+	echo "Copying files..."
+	for dir in ~/Documents/vids/raw/drone/* ;
+	do
+		FILE="$(basename $dir)"
+
+		if [ ! -e /Volumes/WD_BLACK/Cinema/Drone/Pics/"$FILE" ]
+		then
+			if [[ ${dor: -4} == ".JPG" ]]
+			then
+				cp -v "$dir" /Volumes/WD_BLACK/Cinema/Drone/Vids/"${$(basename $dir)%.JPG}".JPG
+			else
+				if [[ ${dir: -4} == ".MP4" ]]
+				then
+					cp -v "$dir" /Volumes/WD_BLACK/Cinema/Drone/Vids/"${$(basename $dir)%.MP4}".MP4
+				fi 
+			fi
+	
+			echo "$FILE does not exist... Copying...";
+		fi
+	done
+else
+	echo "ERROR: WD_BLACK NOT FOUND"
+fi
 
